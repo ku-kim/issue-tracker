4e0cb5cf643c3b1387d0e7dec4996da442f2bf58
yongseongjeon <yongseong.jeon95@gmail.com>
2022-06-16 17:14:05 +0900

    :lipstick: 공통적으로 사용되는 폰트, 색상 모듈화

---
+++ fe/src/styles/font.ts
@@ -0,0 +1,14 @@
+const FONT = {
+  SIZE: {
+    X_LARGE: '32px',
+    LARGE: '24px',
+    MEDIUM: '18px',
+    SMALL: '16px',
+  },
+  WEIGHT: {
+    REGULAR: 400,
+    BOLD: 700,
+  },
+};
+
+export default FONT;
