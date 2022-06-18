159409165919b8190a89fe9235a92585d652cb20
yongseongjeon <yongseong.jeon95@gmail.com>
2022-06-18 21:22:07 +0900

    :sparkles: template 페이지 라우팅

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Template from 'pages/Template';
 import GlobalStyle from 'styles/GlobalStyle';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div />} />
+          <Route path="/template" element={<Template />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
