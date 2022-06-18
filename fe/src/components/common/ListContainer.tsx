49b56b2c2d6807eb9e805e59dc8015f386d471ca
BB-choi <choibobae0809@gmail.com>
2022-06-18 22:26:51 +0900

    :lipstick: ListContainer 스타일 설정

--- fe/src/components/common/ListContainer.tsx
+++ fe/src/components/common/ListContainer.tsx
@@ -16,40 +16,45 @@ const LIST_CONTAINER_SIZE_PIXEL = {
   },
 };
 
+const BORDER_COLOR = COLOR.GREY[300];
+
 const Wrapper = styled.div`
-  width: ${LIST_CONTAINER_SIZE_PIXEL}px;
+  width: ${LIST_CONTAINER_SIZE_PIXEL.WIDTH}px;
   border-radius: ${LIST_CONTAINER_SIZE_PIXEL.BORDER_RADIUS}px;
-  border: 1px solid ${COLOR.GREY[500]};
-  padding: 0 ${LIST_CONTAINER_SIZE_PIXEL.PADDING_LEFT}px;
+  border: 1px solid ${BORDER_COLOR};
+  overflow: hidden;
 
   .list-header {
     width: 100%;
     height: ${LIST_CONTAINER_SIZE_PIXEL.HEADER.HEIGHT}px;
     background-color: ${COLOR.GREY[100]};
     font-size: ${LIST_CONTAINER_SIZE_PIXEL.HEADER.FONT_SIZE}px;
-    padding: ${LIST_CONTAINER_SIZE_PIXEL.HEADER.PADDING_TOP}px 0;
+    padding: ${LIST_CONTAINER_SIZE_PIXEL.HEADER.PADDING_TOP}px
+      ${LIST_CONTAINER_SIZE_PIXEL.PADDING_LEFT}px;
+    border-bottom: 1px solid ${BORDER_COLOR};
   }
 
   .list-body {
     width: 100%;
     background-color: ${COLOR.WHITE};
-  }
 
-  .list-body-item {
-    height: ${LIST_CONTAINER_SIZE_PIXEL.BODY.ITEM_HEIGHT};
-    padding: ${LIST_CONTAINER_SIZE_PIXEL.BODY.PADDING_TOP}px 0;
+    &:nth-child(n) {
+      height: ${LIST_CONTAINER_SIZE_PIXEL.BODY.ITEM_HEIGHT};
+      padding: ${LIST_CONTAINER_SIZE_PIXEL.BODY.PADDING_TOP}px
+        ${LIST_CONTAINER_SIZE_PIXEL.PADDING_LEFT}px;
+
+      &:not(:last-child) {
+        border-bottom: 1px solid ${BORDER_COLOR};
+      }
+    }
   }
 `;
 
-function ListContainer({ headerItem, bodyItems }: ListContainerProps) {
+function ListContainer({ headerItem, children }: ListContainerProps) {
   return (
     <Wrapper>
       <div className="list-header">{headerItem}</div>
-      <div className="list-body">
-        {bodyItems.map((bodyItem) => (
-          <div className="list-body-item">{bodyItem}</div>
-        ))}
-      </div>
+      <div className="list-body">{children}</div>
     </Wrapper>
   );
 }
@@ -58,5 +63,5 @@ export default ListContainer;
 
 interface ListContainerProps {
   headerItem: React.ReactNode;
-  bodyItems: React.ReactNode[];
+  children: React.ReactNode;
 }
