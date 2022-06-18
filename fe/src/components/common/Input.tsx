b60bd7191ccc447c0640e3ba66b258b5dd3e1e2f
BB-choi <choibobae0809@gmail.com>
2022-06-18 16:23:54 +0900

    :sparkles: Input 컴포넌트 기본 구현

---
+++ fe/src/components/common/Input.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import { COLOR } from 'styles/color';
+
+const Wrapper = styled.div<{
+  size: SizeType;
+  isFocused: boolean;
+  templateStyle: TemplateStyleType;
+}>`
+  display: flex;
+  width: ${({ size }) => size.width};
+  height: ${({ size }) => size.height};
+  background-color: ${({ isFocused }) => (isFocused ? COLOR.WHITE : COLOR.GREY[200])};
+  border: 1px solid ${({ isFocused }) => (isFocused ? COLOR.BLACK : 'transparent')};
+
+  .input-label {
+    ${({ templateStyle, isFocused }) => {
+      let inputLabelStyle = ``;
+      switch (templateStyle) {
+        case 'small':
+          break;
+        case 'large':
+        case 'medium':
+          inputLabelStyle += isFocused ? `display: block;` : `display: none`;
+          break;
+        default:
+          break;
+      }
+      return inputLabelStyle;
+    }}
+  }
+
+  .input-wrap {
+  }
+`;
+
+function Input({
+  placeholder,
+  size,
+  InputTitle = '',
+  inputId,
+  templateStyle = 'medium',
+}: InputProps) {
+  const [isFocused, setIsFocused] = useState(false);
+  const [inputText, setInputText] = useState('');
+
+  const onChange = ({ target }: { target: HTMLInputElement }) => {
+    setInputText(target.value);
+  };
+  return (
+    <Wrapper size={size} isFocused={isFocused} templateStyle={templateStyle}>
+      <label className="input-label" htmlFor={inputId}>
+        {InputTitle}
+      </label>
+      <div className="input-wrap">
+        <input
+          placeholder={placeholder}
+          onChange={onChange}
+          value={inputText}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          id={inputId}
+          style={{ background: 'transparent' }}
+        />
+      </div>
+    </Wrapper>
+  );
+}
+
+export default Input;
+
+interface SizeType {
+  width: string;
+  height: string;
+}
+
+type TemplateStyleType = 'small' | 'large' | 'medium';
+
+interface InputProps {
+  placeholder: string;
+  size: SizeType;
+  templateStyle?: TemplateStyleType;
+  inputId: string;
+  InputTitle?: React.ReactNode;
+}
