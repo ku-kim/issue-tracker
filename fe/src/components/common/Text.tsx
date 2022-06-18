46d0708ac042b9d93c1dfe368fa4153e46b0c666
yongseongjeon <yongseong.jeon95@gmail.com>
2022-06-18 21:21:33 +0900

    :sparkles: Text 컴포넌트 추가

---
+++ fe/src/components/common/Text.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import { COLOR } from 'styles/color';
+import FONT from 'styles/font';
+
+function Text({
+  size = FONT.SIZE.SMALL,
+  weight = FONT.WEIGHT.REGULAR,
+  color = COLOR.BLACK,
+  children,
+}: TextProps) {
+  return (
+    <Span size={size} weight={weight} color={color}>
+      {children}
+    </Span>
+  );
+}
+
+type TextProps = {
+  size?: string;
+  weight?: number;
+  color?: string;
+  children: string;
+};
+
+type SpanProps = Omit<TextProps, 'children'>;
+
+const Span = styled.span<SpanProps>`
+  font-size: ${({ size }) => size};
+  font-weight: ${({ weight }) => weight};
+  color: ${({ color }) => color};
+`;
+
+export default Text;
