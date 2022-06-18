70ebdfc742551d4947268ebe6cdfea9568c9fd56
yongseongjeon <yongseong.jeon95@gmail.com>
2022-06-18 20:01:31 +0900

    :lipstick: 스타일 관련 size 파일 모듈화

---
+++ fe/src/styles/size.ts
@@ -0,0 +1,18 @@
+const SIZE = {
+  LOGO: {
+    LARGE: {
+      WIDTH: 342,
+      HEIGHT: 72,
+    },
+    MEDIUM: {
+      WIDTH: 199,
+      HEIGHT: 40,
+    },
+  },
+  ICON: {
+    WIDTH: 16,
+    HEIGHT: 16,
+  },
+};
+
+export default SIZE;
