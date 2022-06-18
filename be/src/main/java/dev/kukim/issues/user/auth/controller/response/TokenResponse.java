8d92962ff31fdbfd127af1a9ee76dd3204319fe0
“kukim” <kukim.dev@gmail.com>
2022-06-18 12:14:38 +0900

    :recycle: Json 카멜 케이스 -> 스네이크 케이스 매핑 통일

--- be/src/main/java/dev/kukim/issues/user/auth/controller/response/TokenResponse.java
+++ be/src/main/java/dev/kukim/issues/user/auth/controller/response/TokenResponse.java
@@ -1,16 +1,16 @@
 package dev.kukim.issues.user.auth.controller.response;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 
 @Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TokenResponse {
-	@JsonProperty("access_token")
+
 	private String accessToken;
 
-	@JsonProperty("scope")
 	private String scope;
 
-	@JsonProperty("token_type")
 	private String tokenType;
 }
