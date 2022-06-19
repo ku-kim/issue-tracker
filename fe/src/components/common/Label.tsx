2318bb5be575700b5ed9416d2dcdfa361b5b1ca1
yongseongjeon <yongseong.jeon95@gmail.com>
2022-06-19 09:56:33 +0900

    :sparkles: Label 컴포넌트 생성

---
+++ fe/src/components/common/Label.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+function Label({ backgroundColor, color, children, borderColor }: LabelProps) {
+  return (
+    <Wrap backgroundColor={backgroundColor} color={color} borderColor={borderColor}>
+      {children}
+    </Wrap>
+  );
+}
+
+const Wrap = styled.div<WrapProps>`
+  width: fit-content;
+  padding: 10px 16px;
+  border-radius: 30px;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  color: ${({ color }) => color};
+  border: ${({ borderColor }) => borderColor && `1px solid ${borderColor}`};
+`;
+
+type LabelProps = {
+  backgroundColor: string;
+  color: string;
+  children: React.ReactNode;
+  borderColor?: string;
+};
+
+type WrapProps = Omit<LabelProps, 'children'>;
+
+export default Label;
