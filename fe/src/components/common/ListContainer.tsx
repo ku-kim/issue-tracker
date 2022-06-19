078e1f2a8896ed0bd0e1b3ac1e6f0b4a4e14c11e
BB-choi <choibobae0809@gmail.com>
2022-06-19 18:56:50 +0900

    :recycle: ListContainer의 size를 size.ts에 지정

--- fe/src/components/common/ListContainer.tsx
+++ fe/src/components/common/ListContainer.tsx
@@ -1,36 +1,21 @@
 import styled from 'styled-components';
 import { COLOR } from 'styles/color';
-
-const LIST_CONTAINER_SIZE_PIXEL = {
-  WIDTH: 1280,
-  PADDING_LEFT: 32,
-  BORDER_RADIUS: 16,
-  HEADER: {
-    HEIGHT: 64,
-    PADDING_TOP: 18,
-    FONT_SIZE: 16,
-  },
-  BODY: {
-    ITEM_HEIGHT: 100,
-    PADDING_TOP: 16,
-  },
-};
+import SIZE from 'styles/size';
 
 const BORDER_COLOR = COLOR.GREY[300];
 
 const Wrapper = styled.div`
-  width: ${LIST_CONTAINER_SIZE_PIXEL.WIDTH}px;
-  border-radius: ${LIST_CONTAINER_SIZE_PIXEL.BORDER_RADIUS}px;
+  width: ${SIZE.LIST_CONTAINER.WIDTH}px;
+  border-radius: ${SIZE.LIST_CONTAINER.BORDER_RADIUS}px;
   border: 1px solid ${BORDER_COLOR};
   overflow: hidden;
 
   .list-header {
     width: 100%;
-    height: ${LIST_CONTAINER_SIZE_PIXEL.HEADER.HEIGHT}px;
+    height: ${SIZE.LIST_CONTAINER.HEADER.HEIGHT}px;
     background-color: ${COLOR.GREY[100]};
-    font-size: ${LIST_CONTAINER_SIZE_PIXEL.HEADER.FONT_SIZE}px;
-    padding: ${LIST_CONTAINER_SIZE_PIXEL.HEADER.PADDING_TOP}px
-      ${LIST_CONTAINER_SIZE_PIXEL.PADDING_LEFT}px;
+    font-size: ${SIZE.LIST_CONTAINER.HEADER.FONT_SIZE}px;
+    padding: ${SIZE.LIST_CONTAINER.HEADER.PADDING_TOP}px ${SIZE.LIST_CONTAINER.PADDING_LEFT}px;
     border-bottom: 1px solid ${BORDER_COLOR};
   }
 
@@ -39,9 +24,8 @@ const Wrapper = styled.div`
     background-color: ${COLOR.WHITE};
 
     &:nth-child(n) {
-      height: ${LIST_CONTAINER_SIZE_PIXEL.BODY.ITEM_HEIGHT};
-      padding: ${LIST_CONTAINER_SIZE_PIXEL.BODY.PADDING_TOP}px
-        ${LIST_CONTAINER_SIZE_PIXEL.PADDING_LEFT}px;
+      height: ${SIZE.LIST_CONTAINER.BODY.ITEM_HEIGHT};
+      padding: ${SIZE.LIST_CONTAINER.BODY.PADDING_TOP}px ${SIZE.LIST_CONTAINER.PADDING_LEFT}px;
 
       &:not(:last-child) {
         border-bottom: 1px solid ${BORDER_COLOR};
