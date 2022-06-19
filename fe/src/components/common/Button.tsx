b7cbf6c76f3de95ce42bec44e69250ab60ea58e8
BB-choi <choibobae0809@gmail.com>
2022-06-19 16:39:39 +0900

    :sparkles: Button 컴포넌트 구현

---
+++ fe/src/components/common/Button.tsx
@@ -0,0 +1,200 @@
+import styled, { css } from 'styled-components';
+import { COLOR } from 'styles/color';
+import FONT from 'styles/font';
+
+const BUTTON_STYLES = {
+  LARGE: {
+    SIZE: {
+      WIDTH: 240,
+      HEIGHT: 64,
+    },
+    FONT_STYLE: {
+      SIZE: FONT.SIZE.MEDIUM,
+      COLOR: COLOR.WHITE,
+      WEIGHT: FONT.WEIGHT.BOLD,
+    },
+    BORDER_STYLE: `
+      border-radius: 20px;
+      border: 0;
+    `,
+  },
+  SMALL_STANDARD: {
+    SIZE: {
+      WIDTH: 120,
+      HEIGHT: 40,
+    },
+    FONT_STYLE: {
+      SIZE: FONT.SIZE.X_SMALL,
+      COLOR: COLOR.WHITE,
+      WEIGHT: FONT.WEIGHT.BOLD,
+    },
+    BORDER_STYLE: `
+      border-radius: 20px;
+      border: 0;
+    `,
+  },
+  SMALL_SECONDARY: {
+    SIZE: {
+      WIDTH: 120,
+      HEIGHT: 40,
+    },
+    FONT_STYLE: {
+      SIZE: FONT.SIZE.X_SMALL,
+      COLOR: COLOR.BLUE[200],
+      WEIGHT: FONT.WEIGHT.BOLD,
+    },
+    BORDER_STYLE: `
+      border-radius: 11px;
+      border: 0;
+    `,
+  },
+  MEDIUM_STANDARD: {
+    SIZE: {
+      WIDTH: 240,
+      HEIGHT: 56,
+    },
+    FONT_STYLE: {
+      SIZE: FONT.SIZE.MEDIUM,
+      COLOR: COLOR.WHITE,
+      WEIGHT: FONT.WEIGHT.BOLD,
+    },
+    BORDER_STYLE: `
+      border-radius: 20px;
+      border: 0;
+    `,
+  },
+  MEDIUM_TEXT: {
+    SIZE: {
+      WIDTH: 87,
+      HEIGHT: 32,
+    },
+    FONT_STYLE: {
+      SIZE: FONT.SIZE.MEDIUM,
+      COLOR: COLOR.GREY[500],
+      WEIGHT: FONT.WEIGHT.BOLD,
+    },
+    BORDER_STYLE: `
+      border: 0;
+    `,
+  },
+  SMALL_TEXT: {
+    SIZE: {
+      WIDTH: 70,
+      HEIGHT: 32,
+    },
+    FONT_STYLE: {
+      SIZE: FONT.SIZE.X_SMALL,
+      COLOR: COLOR.GREY[500],
+      WEIGHT: FONT.WEIGHT.BOLD,
+    },
+    BORDER_STYLE: `
+      border: 0;
+    `,
+  },
+};
+
+function Button({
+  templateStyle,
+  onClick,
+  children,
+  disabled,
+  width = `${BUTTON_STYLES[templateStyle].SIZE.WIDTH}px`,
+  height = `${BUTTON_STYLES[templateStyle].SIZE.HEIGHT}px`,
+  borderStyle = BUTTON_STYLES[templateStyle].BORDER_STYLE,
+  fontStyles = {
+    fontSize: BUTTON_STYLES[templateStyle].FONT_STYLE.SIZE,
+    fontColor: { initial: BUTTON_STYLES[templateStyle].FONT_STYLE.COLOR },
+    fontWeight: BUTTON_STYLES[templateStyle].FONT_STYLE.WEIGHT,
+  },
+  backgroundColor,
+}: ButtonProps) {
+  return (
+    <StyledButton
+      className="ellipsis"
+      onClick={onClick}
+      disabled={disabled}
+      width={width}
+      height={height}
+      borderStyle={borderStyle}
+      fontSize={fontStyles.fontSize}
+      fontWeight={fontStyles.fontWeight}
+      fontColor={fontStyles.fontColor}
+      backgroundColor={backgroundColor}
+    >
+      {children}
+    </StyledButton>
+  );
+}
+
+export default Button;
+
+const StyledButton = styled.button<{
+  width: string;
+  height: string;
+  borderStyle?: string;
+  fontSize?: string;
+  fontColor?: FontColors;
+  fontWeight?: number;
+  backgroundColor: BackGroundColors;
+}>`
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  ${({ borderStyle }) => css` ${borderStyle}}`};
+  background-color: ${({ backgroundColor }) => backgroundColor.initial};
+  font-size: ${({ fontSize }) => fontSize};
+  font-weight: ${({ fontWeight }) => fontWeight};
+  color: ${({ fontColor }) => fontColor?.initial};
+
+  &:not(:disabled):hover {
+    background-color: ${({ backgroundColor }) => backgroundColor.hover};
+    color: ${({ fontColor }) => fontColor?.hover};
+  }
+
+  &:focus {
+    background-color: ${({ backgroundColor }) => backgroundColor.focus};
+  }
+
+  &:active {
+    color: ${({ fontColor }) => fontColor?.active};
+  }
+
+  &:disabled {
+    background-color: ${({ backgroundColor }) => backgroundColor.disabled};
+    color: ${({ fontColor }) => fontColor?.disabled};
+    cursor: default;
+  }
+`;
+
+type TemplateStyleType = keyof typeof BUTTON_STYLES;
+
+interface ButtonProps {
+  onClick?: () => void;
+  children: React.ReactNode;
+  disabled?: boolean;
+  width?: string;
+  height?: string;
+  borderStyle?: string;
+  templateStyle: TemplateStyleType;
+  fontStyles?: FontStyles;
+  backgroundColor: BackGroundColors;
+}
+
+interface FontColors {
+  initial: string;
+  active?: string;
+  hover?: string;
+  disabled?: string;
+}
+
+interface FontStyles {
+  fontSize?: string;
+  fontColor?: FontColors;
+  fontWeight?: number;
+}
+
+interface BackGroundColors {
+  initial: string;
+  hover?: string;
+  focus?: string;
+  disabled?: string;
+}
