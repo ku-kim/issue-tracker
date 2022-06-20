b816f732085ccde70a4545710d5c02024fd2c6da
“kukim” <kukim.dev@gmail.com>
2022-06-21 01:32:14 +0900

    :sparkles: CORS 설정
    - localhost / AWS CloudFront

--- be/src/main/java/dev/kukim/issues/config/WebMvcConfig.java
+++ be/src/main/java/dev/kukim/issues/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package dev.kukim.issues.config;
 import dev.kukim.issues.user.auth.controller.LoginInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,9 +14,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	private final LoginInterceptor loginInterceptor;
 
 	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("http://localhost:3000", "https://d197fs5q8pj3rm.cloudfront.net")
+			.allowedMethods("*");
+	}
+
+	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(loginInterceptor)
-			.addPathPatterns("/**")
-			.excludePathPatterns("/login/**");
+			.addPathPatterns("/issues/**");
 	}
 }
