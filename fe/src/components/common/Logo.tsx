8cb72f56d1a6986bf603bd3c5690ea69ec4cebdb
yongseongjeon <yongseong.jeon95@gmail.com>
2022-06-20 15:27:25 +0900

    :recycle: SIZE 파일 활용하여 타입 지정

--- fe/src/components/common/Logo.tsx
+++ fe/src/components/common/Logo.tsx
@@ -1,19 +1,17 @@
 import Icon from 'components/common/Icon';
 import SIZE from 'styles/size';
 
-function Logo({ size }: LogoProps) {
+function Logo({ size }: { size: LogoSizeType }) {
   switch (size) {
-    case 'large':
+    case 'LARGE':
       return <Icon icon="logo" width={SIZE.LOGO.LARGE.WIDTH} height={SIZE.LOGO.LARGE.HEIGHT} />;
-    case 'medium':
+    case 'MEDIUM':
       return <Icon icon="logo" width={SIZE.LOGO.MEDIUM.WIDTH} height={SIZE.LOGO.MEDIUM.HEIGHT} />;
     default:
       throw new Error(`size: ${size}의 타입이 잘못되었습니다.`);
   }
 }
 
-type LogoProps = {
-  size: string;
-};
+type LogoSizeType = keyof typeof SIZE.LOGO;
 
 export default Logo;
