ac7ff97b7a2f136eccb5a31922c3a8612a596a40
BB-choi <choibobae0809@gmail.com>
2022-06-22 00:49:52 +0900

    :sparkles: LinearGraph 컴포넌트 구현 및 Template 작업

---
+++ fe/src/components/common/LinearGraph.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import { COLOR } from 'styles/color';
+import SIZE from 'styles/size';
+
+function LinearGraph({ totalCount, doneCount }: LinearGraphProps) {
+  if (doneCount > totalCount) {
+    throw new Error('doneCount는 totalCount를 초과하여 입력할 수 없습니다.');
+  }
+
+  const donePercent = totalCount ? (doneCount / totalCount) * 100 : 0;
+
+  return (
+    <Wrapper percent={donePercent}>
+      <div className="filled-graph" />
+    </Wrapper>
+  );
+}
+
+export default LinearGraph;
+
+const Wrapper = styled.div<{ percent: number }>`
+  width: ${SIZE.LINEAR_GRAPH.WIDTH}px;
+  height: ${SIZE.LINEAR_GRAPH.HEIGHT}px;
+  background-color: ${COLOR.GREY[100]};
+  border-radius: 10px;
+  overflow: hidden;
+
+  .filled-graph {
+    width: ${({ percent }) => percent}%;
+    height: 100%;
+    background-color: ${COLOR.BLUE[200]};
+  }
+`;
+
+type LinearGraphProps = {
+  totalCount: number;
+  doneCount: number;
+};
