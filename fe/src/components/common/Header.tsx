139fc7269f9670905ea18d730750d3796f7b2d2c
yongseongjeon <yongseong.jeon95@gmail.com>
2022-06-21 16:19:23 +0900

    :lipstick: Header 컴포넌트 생성

---
+++ fe/src/components/common/Header.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import Avatar from 'components/common/Avatar';
+import Logo from 'components/common/Logo';
+import SIZE from 'styles/size';
+
+function Header({ avatarUrl }: { avatarUrl: string }) {
+  return (
+    <Wrapper className="wrap">
+      <Logo size="MEDIUM" />
+      <Avatar size="LARGE" imgSource={avatarUrl} />
+    </Wrapper>
+  );
+}
+
+export default Header;
+
+const Wrapper = styled.header`
+  height: ${SIZE.HEADER.HEIGHT}px;
+  display: flex;
+  justify-content: space-between;
+`;
