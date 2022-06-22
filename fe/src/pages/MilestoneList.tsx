176aaf5e26e3bf0824de6a9316b3fdd32cf96e7c
BB-choi <choibobae0809@gmail.com>
2022-06-22 15:37:09 +0900

    :sparkles: MilestoneHeader 작성 및 적용

--- fe/src/pages/MilestoneList.tsx
+++ fe/src/pages/MilestoneList.tsx
@@ -1,3 +1,4 @@
+import MilestoneHeader from 'components/MilestoneList/MilestoneHeader';
 import Header from 'components/common/Header';
 import ListContainer from 'components/common/ListContainer';
 import SubNav from 'components/common/SubNav';
@@ -7,7 +8,7 @@ function MilestoneList() {
     <main className="wrap">
       <Header avatarUrl="null" />
       <SubNav location="MILESTONE" />
-      <ListContainer headerItem="안녕하세요">
+      <ListContainer headerItem={<MilestoneHeader />}>
         <div>야호</div>
       </ListContainer>
     </main>
