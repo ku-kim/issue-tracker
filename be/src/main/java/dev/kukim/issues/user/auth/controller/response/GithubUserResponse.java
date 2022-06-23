57c7f7a3e3135798ee4f33302d9fe3aff3bb52c1
“kukim” <kukim.dev@gmail.com>
2022-06-23 21:18:47 +0900

    :recycle: GithubUserResponse 객체 생성 시 setter -> 생성자로 변경

--- be/src/main/java/dev/kukim/issues/user/auth/controller/response/GithubUserResponse.java
+++ be/src/main/java/dev/kukim/issues/user/auth/controller/response/GithubUserResponse.java
@@ -14,4 +14,14 @@ public class GithubUserResponse {
 
 	private String avatarUrl;
 
+	private GithubUserResponse(Long id, String name, String email, String avatarUrl) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.avatarUrl = avatarUrl;
+	}
+
+	public static GithubUserResponse of(Long id, String name, String email, String avatarUrl) {
+		return new GithubUserResponse(id, name, email, avatarUrl);
+	}
 }
