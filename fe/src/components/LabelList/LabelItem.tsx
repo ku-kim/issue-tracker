5bbe4aceeaf72b5718403384712b92cc46a1c720
BB-choi <choibobae0809@gmail.com>
2022-06-23 22:59:48 +0900

    :recycle: TextButtonLink 이용 리팩토링

--- fe/src/components/LabelList/LabelItem.tsx
+++ fe/src/components/LabelList/LabelItem.tsx
@@ -1,8 +1,7 @@
 import styled from 'styled-components';
+import TextButtonLink from 'components/common/Button/TextButtonLink';
 import { COLOR } from 'styles/color';
 import { ellipsisStyle } from 'styles/commonStyles';
-import FONT from 'styles/font';
-import ButtonLink from '../common/Button/ButtonLink';
 import Icon from '../common/Icon';
 import Text from '../common/Text';
 
@@ -12,36 +11,14 @@ function LabelItem({ id, label, desc }: LabelItemProps) {
       <div>{label}</div>
       <LabelDesc color={COLOR.GREY[500]}>{desc}</LabelDesc>
       <ButtonArea>
-        <ButtonLink
-          template="SMALL_TEXT"
-          backgroundColor={{ initial: COLOR.WHITE }}
-          fontStyles={{
-            fontColor: {
-              initial: COLOR.GREY[500],
-            },
-            fontSize: FONT.SIZE.X_SMALL,
-            fontWeight: FONT.WEIGHT.BOLD,
-          }}
-          to={`/${id}`}
-        >
+        <TextButtonLink to={`/${id}`} color={COLOR.GREY[500]}>
           <Icon icon="edit" stroke={COLOR.GREY[500]} />
           편집
-        </ButtonLink>
-        <ButtonLink
-          template="SMALL_TEXT"
-          backgroundColor={{ initial: COLOR.WHITE }}
-          fontStyles={{
-            fontColor: {
-              initial: COLOR.RED[200],
-            },
-            fontSize: FONT.SIZE.X_SMALL,
-            fontWeight: FONT.WEIGHT.BOLD,
-          }}
-          to={`/${id}`}
-        >
+        </TextButtonLink>
+        <TextButtonLink to={`/${id}`} color={COLOR.RED[200]}>
           <Icon icon="trash" stroke={COLOR.RED[200]} />
           삭제
-        </ButtonLink>
+        </TextButtonLink>
       </ButtonArea>
     </Wrapper>
   );
