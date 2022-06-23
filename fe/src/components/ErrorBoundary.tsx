2234da939d4c2ee2d7e418f3764e8f9e811ae41f
yongseongjeon <yongseong.jeon95@gmail.com>
2022-06-23 18:01:40 +0900

    :sparkles: ErrorBoundary 생성

---
+++ fe/src/components/ErrorBoundary.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react';
+
+class ErrorBoundary extends React.Component<{ children: ReactNode }, { hasError: boolean }> {
+  constructor(props: any) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    console.log(error);
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
