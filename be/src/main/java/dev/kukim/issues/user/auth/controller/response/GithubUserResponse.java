f91280d0a9b225daf3f80adf64058e6c8e0d2e24
“kukim” <kukim.dev@gmail.com>
2022-06-24 12:07:05 +0900

    :bug: jackson bind 안되는 문제 해결 (GithubUserResponse 기본 생성자 추가)

--- be/src/main/java/dev/kukim/issues/user/auth/controller/response/GithubUserResponse.java
+++ be/src/main/java/dev/kukim/issues/user/auth/controller/response/GithubUserResponse.java
@@ -1,8 +1,10 @@
 package dev.kukim.issues.user.auth.controller.response;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @Getter @Setter
 public class GithubUserResponse {
 
