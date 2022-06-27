42847f82f4760a9b10df44feded6687071f6c9a3
yongseongjeon <yongseong.jeon95@gmail.com>
2022-06-27 17:16:08 +0900

    :bug: useFetch action type 오타 수정

--- fe/src/pages/Login.tsx
+++ fe/src/pages/Login.tsx
@@ -2,20 +2,15 @@ import styled from 'styled-components';
 import Anchor from 'components/Login/Anchor';
 import Logo from 'components/common/Logo';
 import { flexCenterStyle } from 'styles/commonStyles';
-import useGoodFetch from 'hooks/useGoodFetch';
 
-const authorizeURL =
+const AUTHORIZE_URL =
   'https://github.com/login/oauth/authorize?client_id=dd0ee875caf7ce9d591b&scope=id,name,email,avatar_url';
 
 function Login() {
-  // const fetchedData = useFetch({ url: 'https://reqres.in/api/products/3' });
-  const { data } = useGoodFetch('https://reqres.in/api/products/3');
-  console.log('data', data);
-
   return (
     <Wrapper>
       <Logo size="LARGE" />
-      <Anchor name="GitHub 계정으로 로그인" href={authorizeURL} />
+      <Anchor name="GitHub 계정으로 로그인" href={AUTHORIZE_URL} />
     </Wrapper>
   );
 }
