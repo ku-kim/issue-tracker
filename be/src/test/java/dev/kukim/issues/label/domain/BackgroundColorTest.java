aea16c3637b8b778034e798566688d64b545fbef
“kukim” <kukim.dev@gmail.com>
2022-06-28 00:17:44 +0900

    :white_check_mark: `@Test` -> `@ParameterizedTest` 변경

--- be/src/test/java/dev/kukim/issues/label/domain/BackgroundColorTest.java
+++ be/src/test/java/dev/kukim/issues/label/domain/BackgroundColorTest.java
@@ -6,7 +6,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import dev.kukim.issues.label.exception.InvalidBackgroudColorException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("BackgroudColorTest 클래스")
 class BackgroundColorTest {
@@ -19,32 +21,13 @@ class BackgroundColorTest {
 		@DisplayName("만약 유효한 RGB 문자열 값이 주어진다면")
 		class Context_with_valid_RGB_String {
 
-			@Test
+			@ParameterizedTest
+			@ValueSource(strings = {"#FFFFFF", "FFFFFF"})
 			@DisplayName("객체를 반환한다")
-			void It_returns_a_valid_Object1() {
-				BackgroundColor backgroundColor = new BackgroundColor("#FFFFFF");
+			void It_returns_a_valid_Object1(String hexString) {
+				BackgroundColor backgroundColor = new BackgroundColor(hexString);
 
 				assertThat(backgroundColor.getBackgroundColor()).isEqualTo("FFFFFF");
-			}
-
-			@Test
-			@DisplayName("객체를 반환한다")
-			void It_returns_a_valid_Object2() {
-				BackgroundColor backgroundColor = new BackgroundColor("FFFFFF");
-
-				assertThat(backgroundColor.getBackgroundColor()).isEqualTo("FFFFFF");
-			}
-		}
-
-		@Nested
-		@DisplayName("만약 null이 주어진다면")
-		class Context_with_null {
-
-			@Test
-			@DisplayName("예외를 발생시킨다")
-			void It_throws() {
-				assertThatThrownBy(() -> new BackgroundColor(null)).isInstanceOf(
-					InvalidBackgroudColorException.class);
 			}
 		}
 
@@ -52,19 +35,15 @@ class BackgroundColorTest {
 		@DisplayName("만약 유효하지 않은 RGB 문자열 값이 주어진다면")
 		class Context_with_invalid_RGB_String {
 
-			@Test
+			@ParameterizedTest
+			@NullAndEmptySource
+			@ValueSource(strings = {"#FFFFFFF", "#FF"})
 			@DisplayName("예외를 발생시킨다")
-			void It_throws1() {
-				assertThatThrownBy(() -> new BackgroundColor("#FFFFFFFF")).isInstanceOf(
+			void It_throws(String hexString) {
+				assertThatThrownBy(() -> new BackgroundColor("hexString")).isInstanceOf(
 					InvalidBackgroudColorException.class);
 			}
 
-			@Test
-			@DisplayName("예외를 발생시킨다")
-			void It_throws2() {
-				assertThatThrownBy(() -> new BackgroundColor("#FFFF")).isInstanceOf(
-					InvalidBackgroudColorException.class);
-			}
 		}
 	}
 }
