c118a9600565d7fd63ab3e1faafc1029a62a258c
BB-choi <choibobae0809@gmail.com>
2022-06-28 22:45:16 +0900

    :poop: 로딩, 기본이름 설정, 로그인 실패시 메시지

--- fe/src/hooks/useFetchFromIssueTracker.tsx
+++ fe/src/hooks/useFetchFromIssueTracker.tsx
@@ -1,7 +1,7 @@
 import useFetch from './useFetch';
 
 function useFetchFromIssueTracker({ url, options }: { url: string; options?: RequestInit }) {
-  const { data } = useFetch({ url: `${process.env.REACT_APP_API_END_POINT}${url}`, options });
+  const data = useFetch({ url: `${process.env.REACT_APP_API_END_POINT}${url}`, options });
   return data;
 }
 
