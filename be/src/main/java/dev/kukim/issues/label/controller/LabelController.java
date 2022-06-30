30828f47f753c3ba02806d70058bb8256cb6741f
“kukim” <kukim.dev@gmail.com>
2022-06-30 15:40:28 +0900

    :recycle: delete의 HTTP status 변경 - 200 OK -> 204 No Content

--- be/src/main/java/dev/kukim/issues/label/controller/LabelController.java
+++ be/src/main/java/dev/kukim/issues/label/controller/LabelController.java
@@ -7,6 +7,7 @@ import dev.kukim.issues.label.controller.response.LabelResponse;
 import dev.kukim.issues.label.service.LabelService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -41,6 +43,7 @@ public class LabelController {
 	}
 
 	@DeleteMapping("/{labelId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void removeLabel(@PathVariable Long labelId) {
 		labelService.removeLabel(labelId);
 	}
