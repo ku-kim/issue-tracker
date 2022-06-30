f6e23ef17a9a8c93c878cfc041a99bf25900cd84
BB-choi <choibobae0809@gmail.com>
2022-07-01 03:02:39 +0900

    :sparkles: 라벨 배경 색상 선택 기능 구현

--- fe/src/components/LabelList/BackgroundColorSelect.tsx
+++ fe/src/components/LabelList/BackgroundColorSelect.tsx
@@ -1,14 +1,24 @@
 import styled from 'styled-components';
 import Icon from 'components/common/Icon';
 import Text from 'components/common/Text';
-import { LABEL_COLORS } from 'styles/color';
+import { COLOR, LABEL_COLORS } from 'styles/color';
+
+const FIRST_INDEX = 0;
+const LAST_INDEX = LABEL_COLORS.length - 1;
+
+function getRandomLabelColorsIndex() {
+  return Math.floor(Math.random() * (LAST_INDEX - FIRST_INDEX + 1)) + FIRST_INDEX;
+}
 
 function BackgroundColorSelect({ color, setColor }: BackgroundColorSelectProps) {
   return (
     <>
       <ColorHex>{color}</ColorHex>
-      <RefreshButton type="button" onClick={() => setColor(LABEL_COLORS[5])}>
-        <Icon icon="refresh" />
+      <RefreshButton
+        type="button"
+        onClick={() => setColor(LABEL_COLORS[getRandomLabelColorsIndex()])}
+      >
+        <Icon icon="refresh" stroke={COLOR.GREY[500]} />
       </RefreshButton>
     </>
   );
