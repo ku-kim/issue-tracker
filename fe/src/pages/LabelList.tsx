ff78eec119beb52a84a51e15b0472c0a32b0970e
BB-choi <choibobae0809@gmail.com>
2022-06-30 14:13:05 +0900

    :sparkles: LabelList 구현
    - 서버 data로 화면에 보여주도록 구현

--- fe/src/pages/LabelList.tsx
+++ fe/src/pages/LabelList.tsx
@@ -1,64 +1,58 @@
+import { useState } from 'react';
 import styled from 'styled-components';
-import LabelItem, { LabelItemProps } from 'components/LabelList/LabelItem';
+import LabelFormField from 'components/LabelList/LabelFormField';
+import LabelItem, { LabelDataType } from 'components/LabelList/LabelItem';
 import BlankMessage from 'components/common/BlankMessage';
+import Error from 'components/common/Error';
 import Header from 'components/common/Header';
-import Label from 'components/common/Label';
 import ListContainer from 'components/common/ListContainer';
+import Loading from 'components/common/Loading';
 import SubNav from 'components/common/SubNav';
 import Text from 'components/common/Text';
+import useFetchFromIssueTracker from 'hooks/useFetchFromIssueTracker';
 import { COLOR } from 'styles/color';
 import { wrapStyle } from 'styles/commonStyles';
 import FONT from 'styles/font';
 
-const LABEL_MOCK_DATA: LabelItemProps[] = [
-  {
-    id: '123',
-    label: (
-      <Label color="white" backgroundColor="blue">
-        라벨1
-      </Label>
-    ),
-    desc: '123 라벨에 대한 설명',
-  },
-  {
-    id: '234',
-    label: (
-      <Label color="white" backgroundColor="green">
-        label test
-      </Label>
-    ),
-    desc: 'label test 라벨에 대한 설명',
-  },
-  {
-    id: '343',
-    label: (
-      <Label color="white" backgroundColor="red">
-        또 다른 라벨
-      </Label>
-    ),
-    desc: '또 다른 라벨에 대한 설명',
-  },
-];
-
-const dataCount = LABEL_MOCK_DATA.length;
-const hasData = dataCount > 0;
-
 function LabelList() {
+  const [isActiveLabelFormField, setIsActiveLabelFormField] = useState(false);
+
+  const { data, error } = useFetchFromIssueTracker({ url: `api/labels/` });
+
+  if (error) {
+    return <Error message="데이터 통신에 실패했습니다." />;
+  }
+
+  if (!data) {
+    return <Loading />;
+  }
+
+  const { labelsCount, openedMilestonesCount, labels } = data;
+  const hasData = labels?.length > 0;
+
   return (
     <Main>
       <Header avatarUrl="null" />
-      <SubNav location="LABEL" labelCount={0} milestoneCount={0} />
+      <SubNav
+        location="LABEL"
+        labelCount={labelsCount}
+        milestoneCount={openedMilestonesCount}
+        isActiveFormField={isActiveLabelFormField}
+        setIsActiveFormField={setIsActiveLabelFormField}
+      />
+      {isActiveLabelFormField && <LabelFormField />}
+
       <ListContainer
         headerItem={
           <Text weight={FONT.WEIGHT.BOLD} color={COLOR.GREY[500]}>
-            {dataCount}개의 레이블
+            {labelsCount}개의 레이블
           </Text>
         }
       >
         {(hasData &&
-          LABEL_MOCK_DATA.map(({ id, label, desc }) => (
-            <LabelItem id={id} key={id} label={label} desc={desc} />
-          ))) || <BlankMessage text="등록된 라벨이 없습니다" />}
+          labels?.map((labelItemData: LabelDataType) => <LabelItem data={labelItemData} />)) || (
+          <BlankMessage text="등록된 라벨이 없습니다" />
+        )}
       </ListContainer>
     </Main>
   );
