9d40253224c4c3966c602998d6ca041a96d26aee
yongseongjeon <yongseong.jeon95@gmail.com>
2022-07-01 10:31:38 +0900

    :sparkles: :poop: usePost 커스텀 훅 생성
    - 아직 동작 안함

--- fe/src/hooks/useFetchFromIssueTracker.tsx
+++ fe/src/hooks/useFetchFromIssueTracker.tsx
@@ -1,8 +1,7 @@
 import useFetch from './useFetch';
 
-function useFetchFromIssueTracker({ url, options }: { url: string; options?: RequestInit }) {
-  const data = useFetch({ url: `${process.env.REACT_APP_API_END_POINT}${url}`, options });
-  return data;
+function useFetchFromIssueTracker({ url, options }: { url: string; options?: any }) {
+  return useFetch({ url: `${process.env.REACT_APP_API_END_POINT}${url}`, options });
 }
 
 export default useFetchFromIssueTracker;
